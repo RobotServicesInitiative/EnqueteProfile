bcc830f2e894ad22ccb1d29a6d5432a80ea90685
RobotServicesInitiative <toru_izui+rsi@evolveit.jp>
2016-09-29 15:10:02 +0900

    XML生成ロジック追加

---
+++ EnqueteProfile_common/src/enquete_profile/common/CreateAnswerXMLMain.java
@@ -0,0 +1,50 @@
+package enquete_profile.common;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.robotservices.custom_profile.lib.common.util.XMLUtil;
+
+
+public class CreateAnswerXMLMain {
+
+	public static void main(String[] args) {
+
+		AnswerData answerData = new AnswerData();
+
+		answerData.setEnquete_id("enquete_id");
+		answerData.setIetf_language_tag("ja");
+		answerData.setStart_answer_data_date(new Date());
+
+		answerData.setAnswer_list(new ArrayList<Answer>());
+
+		answerData.getAnswer_list().add(createAnswer("question_1","question_1_answer_3","question_1_アンサー_3",null,"question_2"));
+		answerData.getAnswer_list().add(createAnswer("question_2","question_2_answer_2","question_1_アンサー_2",null,"question_3"));
+		answerData.getAnswer_list().add(createAnswer("question_3","question_3_answer_1","question_1_アンサー_1","enquete_12",null));
+
+		answerData.setEnd_answer_data_date(new Date());
+
+		String xml = XMLUtil.marshal(answerData);
+		System.out.println(xml);
+
+		return ;
+	}
+
+	private static Answer createAnswer(String questionId,String answerId,String asnwerString,String nextEnqueteId,String nextQuestionId) {
+
+		Answer answer = new Answer();
+
+		answer.setQuestion_id(questionId);
+		answer.setStart_answer_date(new Date());
+
+		answer.setAnswer_block(new AnswerBlock());
+		answer.getAnswer_block().setAnswer_id(answerId);
+		answer.getAnswer_block().setAnswer_string(asnwerString);
+		answer.getAnswer_block().setNext_enquete_id(nextEnqueteId);
+		answer.getAnswer_block().setNext_question_id(nextQuestionId);
+
+		answer.setEnd_answer_date(new Date());
+
+		return answer;
+	}
+
+}
---
+++ EnqueteProfile_common/src/enquete_profile/common/CreateEnqueteXMLMain.java
@@ -0,0 +1,86 @@
+package enquete_profile.common;
+import java.util.ArrayList;
+
+import org.robotservices.custom_profile.lib.common.util.XMLUtil;
+
+import enquete_profile.common.AnswerBlock;
+import enquete_profile.common.Enquete;
+import enquete_profile.common.EnqueteData;
+
+
+public class CreateEnqueteXMLMain {
+
+	public static void main(String[] args) {
+
+		EnqueteData enqueteData = new EnqueteData();
+
+		enqueteData.setEnquete_id("enquete_1");
+		enqueteData.setBefore_enquete_annotation("アンケート前注釈");
+		enqueteData.setAfter_enquete_annotation("アンケート後注釈");
+		enqueteData.setIetf_language_tag("ja");
+		enqueteData.setEnqueteList(new ArrayList<Enquete>());
+
+		String questionId = "question_1";
+		String nextEnqueteId = null;
+		String nextQuestionId = "question_2";
+		createEnquete(enqueteData,questionId,nextEnqueteId,nextQuestionId);
+
+		questionId = "question_2";
+		nextEnqueteId = null;
+		nextQuestionId = "question_3";
+		createEnquete(enqueteData,questionId,nextEnqueteId,nextQuestionId);
+
+		questionId = "question_3";
+		nextEnqueteId = "enquete_12";
+		nextQuestionId = null;
+		createEnquete(enqueteData,questionId,nextEnqueteId,nextQuestionId);
+
+		String xml = XMLUtil.marshal(enqueteData);
+		System.out.println(xml);
+
+		return ;
+	}
+
+	private static void createEnquete(EnqueteData enqueteData,String questionId,String nextEnqueteId,String nextQuestionId) {
+
+		Enquete enquete1 = new Enquete();
+		enqueteData.getEnqueteList().add(enquete1);
+
+		enquete1.setQuestion_id(questionId);
+		enquete1.setBefore_question_annotation(String.format("%s前注釈",questionId));
+		enquete1.setAfter_question_annotation(String.format("%s後注釈",questionId));
+		enquete1.setQuestion(String.format("%sの質問",questionId));
+		enquete1.setAnswers(new ArrayList<AnswerBlock>());
+		enquete1.setAttract_mode_url(String.format("http://%s", questionId));
+
+
+		{
+			AnswerBlock answerBlock = new AnswerBlock();
+			answerBlock.setAnswer_id(String.format("%s_answer_1",questionId));
+			answerBlock.setAnswer_string(String.format("%s_アンサー_1",questionId));
+			answerBlock.setNext_enquete_id(nextEnqueteId);
+			answerBlock.setNext_question_id(nextQuestionId);
+			enquete1.getAnswers().add(answerBlock);
+		}
+
+		{
+			AnswerBlock answerBlock = new AnswerBlock();
+			answerBlock.setAnswer_id(String.format("%s_answer_2",questionId));
+			answerBlock.setAnswer_string(String.format("%s_アンサー_2",questionId));
+			answerBlock.setNext_enquete_id(nextEnqueteId);
+			answerBlock.setNext_question_id(nextQuestionId);
+			enquete1.getAnswers().add(answerBlock);
+		}
+
+		{
+			AnswerBlock answerBlock = new AnswerBlock();
+			answerBlock.setAnswer_id(String.format("%s_answer_3",questionId));
+			answerBlock.setAnswer_string(String.format("%s_アンサー_3",questionId));
+			answerBlock.setNext_enquete_id(nextEnqueteId);
+			answerBlock.setNext_question_id(nextQuestionId);
+			enquete1.getAnswers().add(answerBlock);
+		}
+
+	}
+
+}
